d409cac339d36665132b8d14157a85369f63251c
imroh17kadam <kadam17rohit@gmail.com>
2025-12-24 17:38:20 +0530

    Files removal and addition changes

--- warehouse/verify_feature_table.py
+++
@@ -1,5 +0,0 @@
-import duckdb
-
-con = duckdb.connect("warehouse/cola_warehouse.duckdb")
-print(con.execute("SELECT * FROM features.demand_forecasting LIMIT 5").fetchdf())
-con.close()
--- warehouse/verify_tables.py
+++
@@ -1,6 +0,0 @@
-import duckdb
-
-con = duckdb.connect("warehouse/cola_warehouse.duckdb")
-tables = con.execute("SHOW TABLES FROM raw;").fetchall()
-print(tables)
-con.close()
